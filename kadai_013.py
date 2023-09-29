5b4331bb5ad60a33ac2079dcf5d74738a37cb40a
amwdb <simpleisbest916@gmail.com>
2023-09-29 12:11:37 +0900

    Update kadai_013.py

--- kadai_013.py
+++ kadai_013.py
@@ -1,7 +1,7 @@
 #商品を購入して、消費税を加えた計算結果を返す関数を記述してください。
 #第1引数に商品の金額、第2引数に消費税（10%）を設定できるようにしてください。
 
-def purchase_item(price,tax_price) ->int:
+def purchase_item(price,tax_price):
     total_price=price*(1 + (tax_price/100))
     print(f"商品金額は{total_price}円です。")
 
